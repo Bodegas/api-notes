0b69559b25bbfb65215c6a938c523e86f0d41642
David <bodegas.mail@gmail.com>
2021-08-02 02:16:23 +0200

    Add express

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const http = require("http");
+const express = require("express");
+const { request } = require("http");
+const app = express();
 
-const data = [
+let contents = [
   { id: 1, content: "Content 1", color: "red" },
   {
     id: 2,
@@ -9,10 +11,33 @@ const data = [
   },
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  response.end(JSON.stringify(data));
+app.get("/", (request, response) => {
+  response.send("<h1>Api molona</h1>");
+});
+
+app.get("/api/contents", (request, response) => {
+  response.json(contents);
+});
+
+app.get("/api/contents/:id", (request, response) => {
+  const id = request.params.id;
+  const content = contents.find(content => content.id.toString() === id);
+  if(content){
+    response.json(content);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/contents/:id", (request, response) => {
+  const id = request.params.id;
+  contents = contents.filter(content => content.id.toString() !== id);
+  if(contents){
+    response.json(contents);
+  } else {
+    response.status(404).end();
+  }
 });
 
 const PORT = 3001;
-app.listen(PORT);
+app.listen(PORT, () => console.log(`Server runnig on port ${PORT}`));
