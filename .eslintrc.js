3fd112d6b6be86b43ee96698a2bcb91c05b9717c
David <bodegas.mail@gmail.com>
2021-08-03 23:43:53 +0200

    Add eslint + prettier.

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "prettier/prettier": "error",
+  },
+};
