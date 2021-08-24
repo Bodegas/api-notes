2b28e8517c94396d62a0103abf929a7be3cfef55
David <bodegas.mail@gmail.com>
2021-08-25 00:59:18 +0200

    Refactor index to use controllers for notes.

---
+++ src/controllers/notes.js
@@ -0,0 +1,77 @@
+const notesRouter = require("express").Router();
+const Note = require("../models/Note.js");
+const User = require("../models/User.js");
+
+notesRouter.get("/", (request, response, next) => {
+  Note.find({})
+    .then(notes => response.json(notes))
+    .catch(error => next(error));
+});
+
+notesRouter.get("/:id", (request, response, next) => {
+  const { id } = request.params;
+  Note.findById(id)
+    .then(note => {
+      note
+        ? response.json(note)
+        : response.status(404).send({ error: "Note not found" });
+    })
+    .catch(error => next(error));
+});
+
+notesRouter.delete("/:id", async (request, response, next) => {
+  const { id } = request.params;
+  Note.findByIdAndDelete(id)
+    .then(note =>
+      note
+        ? response.status(204).end()
+        : response.status(404).send({ error: "Note not found" })
+    )
+    .catch(error => next(error));
+});
+
+notesRouter.post("/", async (request, response) => {
+  const body = request.body;
+  if (!body.content) {
+    return response.status(400).json({ error: "Content field is required" });
+  }
+  if (!body.user) {
+    return response.status(400).json({ error: "User field is required" });
+  }
+
+  const user = await User.find({ id: request.body.user });
+  if (!user) {
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  const note = new Note({
+    content: body.content,
+    date: new Date(),
+    important: body.important,
+    user: user._id,
+  });
+
+  note
+    .save()
+    .then(() => response.status(201).end())
+    .catch(() => response.status(500).end());
+});
+
+notesRouter.put("/:id", (request, response, next) => {
+  const { id } = request.params;
+  const body = request.body;
+
+  Note.findByIdAndUpdate(
+    id,
+    {
+      ...body,
+    },
+    { new: true }
+  )
+    .then(updatedNote => {
+      response.status(200).json(updatedNote);
+    })
+    .catch(error => next(error));
+});
+
+module.exports = notesRouter;
--- src/index.js
+++ src/index.js
@@ -3,92 +3,19 @@ require("./mongo");
 
 const express = require("express");
 const cors = require("cors");
-// const logger = require("./middleware/loggerMiddleware");
 const handleErrors = require("./middleware/handleError");
 const notFound = require("./middleware/notFound");
-const Note = require("./models/Note");
-const User = require("./models/User");
+const notesRouter = require("./controllers/notes");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-// app.use(logger);
 
 app.get("/", (request, response) => {
   response.send("<h1>Api molona</h1>");
 });
 
-app.get("/api/notes", (request, response, next) => {
-  Note.find({})
-    .then(notes => response.json(notes))
-    .catch(error => next(error));
-});
-
-app.get("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
-  Note.findById(id)
-    .then(note => {
-      note
-        ? response.json(note)
-        : response.status(404).send({ error: "Note not found" });
-    })
-    .catch(error => next(error));
-});
-
-app.delete("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
-  Note.findByIdAndDelete(id)
-    .then(note =>
-      note
-        ? response.status(204).end()
-        : response.status(404).send({ error: "Note not found" })
-    )
-    .catch(error => next(error));
-});
-
-app.post("/api/notes", async (request, response) => {
-  const body = request.body;
-  if (!body.content) {
-    return response.status(400).json({ error: "Content field is required" });
-  }
-  if (!body.user) {
-    return response.status(400).json({ error: "User field is required" });
-  }
-
-  const user = await User.find({ id: request.body.user });
-  if (!user) {
-    return response.status(400).json({ error: "User not found" });
-  }
-
-  const note = new Note({
-    content: body.content,
-    date: new Date(),
-    important: body.important,
-    user: user._id,
-  });
-
-  note
-    .save()
-    .then(() => response.status(201).end())
-    .catch(() => response.status(500).end());
-});
-
-app.put("/api/notes/:id", (request, response, next) => {
-  const { id } = request.params;
-  const body = request.body;
-
-  Note.findByIdAndUpdate(
-    id,
-    {
-      ...body,
-    },
-    { new: true }
-  )
-    .then(updatedNote => {
-      response.status(200).json(updatedNote);
-    })
-    .catch(error => next(error));
-});
+app.use("/api/notes", notesRouter);
 
 app.use(notFound);
 app.use(handleErrors);
